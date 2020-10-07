385808ea488bbe60ada55a1f356005b6161a56ee
Jason Oh <jason@mjc1.etri.re.kr>
2020-10-07 15:26:17 +0900

    First submit

---
+++ helloworld.cc
@@ -0,0 +1,15 @@
+/*
+* helloworld.cc
+ *
+ *  Created on: 2020. 9. 24.
+ *      Author: jason
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello world~!" << endl;
+	return 0;
+}
